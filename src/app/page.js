e0ef867cf8a117f4aaa24de0c5b5663f68c115b7
NolanEmery <nolanemery1206@gmail.com>
2025-07-11 15:07:26 -0700

    first update

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,60 @@
 import { useState } from 'react';
 
 export default function Home() {
-  const [arr, setArr] = useState([]);
+  const [arr, setArr] = useState([]); //need a dynamic number of objects
+  // cannot use one start variable for each of them (because then we would have to instantiate an 
+  // arbtrary number)
+  // CAN use one state variable for the array (but likely need to declare the object)
 
+  //Create
   function addItem(formData) {
     const toAdd = formData.get("addItem");
-    setArr([...arr, toAdd]);
+    setArr([...arr, {
+      thingToDo: toAdd,
+      date: "",
+      time: ""
+    }]);
   }
 
+  // Update
+  function addDate(formData) { //we need the value of the object and the date we are adding
+    const toAddThingToDo = formData.get("addThingToDo");
+    const toAddDate = formData.get("addDate");
+    setArr(arr.map(a => {
+      if (a.thingToDo === toAddThingToDo) {
+        return ({
+          thingToDo: a.thingToDo,
+          date: toAddDate,
+          time: a.time
+        });
+      } else {
+        return a;
+      }
+    }));
+  }
+
+  // Update
+  function addTime(formData) {
+    const toAddThingToDo = formData.get("thingToDo");
+    const toAddTime = formData.get("addTime");
+    setArr(arr.map(a => {
+      if (a.thingToDo === toAddThingToDo) {
+        return ({
+          thingToDo: a.thingToDo,
+          date: a.date,
+          time: toAddTime
+        });
+      } else {
+        return a;
+      }
+    }));
+  }
+
+  // Delete
   function removeItem(formData) {
     const toRemove = formData.get("removeItem");
     setArr(arr.filter(a => {
-      if (a !== toRemove) {
+      if (a.thingToDo !== toRemove) {
         return a;
       }
     }));
@@ -32,11 +75,41 @@ export default function Home() {
     <p>Print out all of your todo items: </p>
     <button onClick={e => {
       for (let i = 0; i < arr.length; i++) {
-        alert(arr[i]);
+        alert(arr[i].thingToDo);
+        if (arr[i].date !== "") {
+          alert(arr[i].date);
+        }
+        if (arr[i].time !== "") {
+          alert(arr[i].time);
+        }
       }
     }}>
       Print
     </button>
+    <p>Add a date to an item: </p>
+    <form action={addDate}>
+    <p>Item: </p>
+    <input name="addThingToDo">
+    </input>
+    <p>Date: </p>
+    <input name="addDate">
+    </input>
+    <button type="submit">
+      Add Date
+    </button>
+    </form>
+    <p>Add a time to an item: </p>
+    <form action={addTime}>
+    <p>Item: </p>
+    <input name="thingToDo">
+    </input>
+    <p>Time: </p>
+    <input name="addTime">
+    </input>
+    <button type="submit">
+      Add Time
+    </button>
+    </form>
     <p>What is the item you would like to remove?</p>
     <form action={removeItem}>
     <input name="removeItem">
