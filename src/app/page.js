058a7efc46506f42218e1525c266aa416f716ab5
NolanEmery <nolanemery1206@gmail.com>
2025-08-27 13:45:55 -0700

    thirteenth version of the app

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 'use client'
-import { useReducer, useRef } from 'react';
+import { useReducer, useRef, useState } from 'react';
 import { Provider } from "@/components/ui/provider";
-import { Tabs, Button } from "@chakra-ui/react";
+import { Tabs, Button, ActionBar, Portal, Wrap, DataList, QrCode, Center } from "@chakra-ui/react";
+import { RiArrowRightLine } from 'react-icons/ri';
 
 export default function Home() {
   const [arr, dispatch] = useReducer(arrReducer, []);
@@ -13,6 +14,8 @@ export default function Home() {
   let dateThingToDoToAdd = useRef("");
   let timeThingToDoToAdd = useRef("");
   let thingToDoToRemove = useRef("");
+
+  const [actionBarState, setActionBarState] = useState(false);
 
   //Create
   function addItem(formData) {
@@ -54,63 +57,164 @@ export default function Home() {
   }
 
   // Read
-  const mondayItems = arr.filter(a1 => a1.date == "Monday").map(a2 => a2.thingToDo + " " + a2.time);
+  const mondayItems = arr.filter(a1 => a1.date == "Monday");
 
   // Read
-  const tuesdayItems = arr.filter(a1 => a1.date == "Tuesday").map(a2 => a2.thingToDo + " " + a2.time);
+  const tuesdayItems = arr.filter(a1 => a1.date == "Tuesday");
 
   // Read
-  const wednesdayItems = arr.filter(a1 => a1.date == "Wednesday").map(a2 => a2.thingToDo + " " + a2.time);
+  const wednesdayItems = arr.filter(a1 => a1.date == "Wednesday");
 
   // Read
-  const thursdayItems = arr.filter(a1 => a1.date == "Thursday").map(a2 => a2.thingToDo + " " + a2.time);
+  const thursdayItems = arr.filter(a1 => a1.date == "Thursday");
 
   // Read
-  const fridayItems = arr.filter(a1 => a1.date == "Friday").map(a2 => a2.thingToDo + " " + a2.time);
+  const fridayItems = arr.filter(a1 => a1.date == "Friday");
 
   // Read
-  const saturdayItems = arr.filter(a1 => a1.date == "Saturday").map(a2 => a2.thingToDo + " " + a2.time);
+  const saturdayItems = arr.filter(a1 => a1.date == "Saturday");
 
   // Read
-  const sundayItems = arr.filter(a1 => a1.date == "Sunday").map(a2 => a2.thingToDo + " " + a2.time);
+  const sundayItems = arr.filter(a1 => a1.date == "Sunday");
 
-  const printedArr = arr.map(a => {
-    return (
-      <p onClick={() => {
-        dateThingToDoToAdd.current = a.thingToDo;
-        timeThingToDoToAdd.current = a.thingToDo;
-        thingToDoToRemove.current = a.thingToDo;
-      }} key={a.thingToDo}>{a.thingToDo + " " + a.date + " " + a.time}</p>
-    );
-  });
+  const printedArr = (
+    <DataList.Root orientation="horizontal">
+      {arr.map(a => (
+        <DataList.Item key={a.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = a.thingToDo;
+          timeThingToDoToAdd.current = a.thingToDo;
+          thingToDoToRemove.current = a.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{a.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Date:</DataList.ItemLabel>
+          <DataList.ItemValue>{a.date}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{a.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
-  const printedMondayItems = mondayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
 
-  const printedTuesdayItems = tuesdayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
+  const printedMondayItems = (
+    <DataList.Root>
+      {mondayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
-  const printedWednesdayItems = wednesdayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
+  const printedTuesdayItems = (
+    <DataList.Root>
+      {tuesdayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
-  const printedThursdayItems = thursdayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
+  const printedWednesdayItems = (
+    <DataList.Root>
+      {wednesdayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
-  const printedFridayItems = fridayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
+  const printedThursdayItems = (
+    <DataList.Root>
+      {thursdayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
-  const printedSaturdayItems = saturdayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
+  const printedFridayItems = (
+    <DataList.Root>
+      {fridayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
-  const printedSundayItems = sundayItems.map(m => {
-    return <p key={m}>{m}</p>;
-  });
+  const printedSaturdayItems = (
+    <DataList.Root>
+      {saturdayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
+
+  const printedSundayItems = (
+    <DataList.Root>
+      {sundayItems.map(m => (
+        <DataList.Item key={m.thingToDo} onClick={() => {
+          dateThingToDoToAdd.current = m.thingToDo;
+          timeThingToDoToAdd.current = m.thingToDo;
+          thingToDoToRemove.current = m.thingToDo;
+        }}>
+          <DataList.ItemLabel>Thing To Do:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.thingToDo}</DataList.ItemValue>
+          <DataList.ItemLabel>Time:</DataList.ItemLabel>
+          <DataList.ItemValue>{m.time}</DataList.ItemValue>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
+  );
 
   return (
     <html suppressHydrationWarning={true}>
@@ -141,53 +245,80 @@ export default function Home() {
             <Tabs.Content value="saturday">{printedSaturdayItems}</Tabs.Content>
             <Tabs.Content value="sunday">{printedSundayItems}</Tabs.Content>
           </Tabs.Root>
-          {/* <> */}
-          <p>What is the item you would like to add?</p>
-          <form action={addItem}>
-            <input name="addItem">
-            </input>
-            <Button type="submit">
-              Add
-            </Button>
-          </form>
+          <Wrap>
+            <p>What is the item you would like to add?</p>
+            <form action={addItem}>
+              <Wrap>
+                <input name="addItem">
+                </input>
+                <Button type="submit">
+                  Add
+                  <RiArrowRightLine />
+                </Button>
+              </Wrap>
+            </form>
+          </Wrap>
+          <Button onClick={() => {
+            setActionBarState(!actionBarState);
+          }}>Click for Further Options</Button>
           {printedArr}
-          <p>Add a date to an item: </p>
-          <form action={addDate}>
-            <label>Date: </label>
-            <label>Monday</label>
-            <input type="radio" name="addDate" value="Monday"></input>
-            <label>Tuesday</label>
-            <input type="radio" name="addDate" value="Tuesday"></input>
-            <label>Wednesday</label>
-            <input type="radio" name="addDate" value="Wednesday"></input>
-            <label>Thursday</label>
-            <input type="radio" name="addDate" value="Thursday"></input>
-            <label>Friday</label>
-            <input type="radio" name="addDate" value="Friday"></input>
-            <label>Saturday</label>
-            <input type="radio" name="addDate" value="Saturday"></input>
-            <label>Sunday</label>
-            <input type="radio" name="addDate" value="Sunday"></input>
-            <Button type="submit">
-              Add date
-            </Button>
-          </form>
-          <p>Add a time to an item: </p>
-          <form action={addTime}>
-            <label>Time: </label>
-            <input name="addTime">
-            </input>
-            <Button type="submit">
-              Add Time
-            </Button>
-          </form>
-          <p>What is the item you would like to remove?</p>
-          <form action={removeItem}>
-            <Button type="submit">
-              Remove
-            </Button>
-          </form>
-          {/* </> */}
+          <ActionBar.Root open={actionBarState} lazyMount unmountOnExit>
+            <Portal>
+              <ActionBar.Positioner>
+                <ActionBar.Content>
+                  <div>
+                    <p>Add a date to an item: </p>
+                    <form action={addDate}>
+                      <label>Date: </label>
+                      <label>Monday</label>
+                      <input type="radio" name="addDate" value="Monday"></input>
+                      <label>Tuesday</label>
+                      <input type="radio" name="addDate" value="Tuesday"></input>
+                      <label>Wednesday</label>
+                      <input type="radio" name="addDate" value="Wednesday"></input>
+                      <label>Thursday</label>
+                      <input type="radio" name="addDate" value="Thursday"></input>
+                      <label>Friday</label>
+                      <input type="radio" name="addDate" value="Friday"></input>
+                      <label>Saturday</label>
+                      <input type="radio" name="addDate" value="Saturday"></input>
+                      <label>Sunday</label>
+                      <input type="radio" name="addDate" value="Sunday"></input>
+                      <Button type="submit">
+                        Add date <RiArrowRightLine />
+                      </Button>
+                    </form>
+                  </div>
+                  <div>
+                    <p>Add a time to an item: </p>
+                    <form action={addTime}>
+                      <label>Time: </label>
+                      <input name="addTime">
+                      </input>
+                      <Button type="submit">
+                        Add Time <RiArrowRightLine />
+                      </Button>
+                    </form>
+                  </div>
+                  <div>
+                    <p>What is the item you would like to remove?</p>
+                    <form action={removeItem}>
+                      <Button type="submit">
+                        Remove
+                      </Button>
+                    </form>
+                  </div>
+                </ActionBar.Content>
+              </ActionBar.Positioner>
+            </Portal>
+          </ActionBar.Root>
+          <Center>
+            <QrCode.Root value="https://github.com/NolanEmery/TodoListApp" size="2xs">
+              <QrCode.Frame>
+                <QrCode.Pattern />
+              </QrCode.Frame>
+            </QrCode.Root>
+          </Center>
         </Provider>
       </body>
     </html>
